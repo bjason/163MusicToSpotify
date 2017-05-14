a66c27a24a9163618905e6988b8a5a2b0a198320
bjason <leiyuchu@hotmail.com>
2017-05-14 21:01:32 +0800

    Add files via upload
    change the playlistId variable in t.py to get the list of your songs

---
+++ t.py
@@ -0,0 +1,24 @@
+import urllib, json
+
+#change the playlistId variable 
+playlistId = 21329331
+url = "http://music.163.com/api/playlist/detail?id="\
+	+ str(playlistId) + "&updateTime=-1"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+
+output = ""
+
+tracks = data["result"]["tracks"]
+for track in tracks:
+	trackName = track["name"]
+	artist = track["artists"][0]["name"]
+	output += trackName + ' - ' + artist + '\n'
+playlistName = data["result"]["name"]
+with open(playlistName + '.txt', 'w') as file:
+	file.write(output.encode('utf8'))
+
+
+
+
+
