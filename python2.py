8bde731805257d078a5bae8f30f38f9379c4ad4a
Pastèque <519134691@qq.com>
2017-08-02 11:14:26 +0800

    Update and rename t.py to python2.py

--- python2.py
+++ python2.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from urllib import urlopen
 
 enterId = 'Enter the playlist id (Enter ? to get help):'
 help = 'To get the id of the playlist, go to the page\
@@ -9,15 +10,7 @@ of it and look at the address bar.\
 errRetrive = 'No data retrived. \nPlease check the playlist id again.'
 
 while 1:
-	if sys.version_info[0] == 3:
-		playlistId = input(enterId)
-		from urllib.request import urlopen
-	else:
-		# Not Python 3 - today, it is most likely to be Python 2
-		# But note that this might need an update when Python 4
-		# might be around one day
-		playlistId = raw_input(enterId)
-		from urllib import urlopen
+	playlistId = raw_input(enterId)
 
 	#change the playlistId variable 
 	if playlistId == '?':
@@ -25,11 +18,7 @@ while 1:
 	urladd = "http://music.163.com/api/playlist/detail?id="\
 		+ str(playlistId) + "&updateTime=-1"
 	# Your code where you can use urlopen
-	if sys.version_info[0] == 3:
-		with urlopen(urladd) as url:
-			response = url.read()
-	else:
-		response = urlopen(urladd).read()
+	response = urlopen(urladd).read()
 	data = json.loads(response)
 
 	output = ""
@@ -45,13 +34,8 @@ while 1:
 		artist = track["artists"][0]["name"]
 		output += trackName + ' - ' + artist + '\n'
 	playlistName = data["result"]["name"]
+	
+	with open(playlistName + '.txt', 'w') as file:
+		file.write(output.encode('utf8'))
 
-	if sys.version_info[0] == 3:
-		with open(playlistName + '.txt', 'wb') as file:
-			file.write(output.encode('utf8'))
-			file.close
-	else:
-		with open(playlistName + '.txt', 'w') as file:
-			file.write(output.encode('utf8'))
-			file.close
 	print('Success.\nCheck the directory of this file and find the .kgl file!')
