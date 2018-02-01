6b9db63782302136af70e461869d468e4a4eaf38
bjason <leiyuchu@hotmail.com>
2018-02-01 16:17:43 +0800

    Update python2.py

--- python2.py
+++ python2.py
@@ -2,6 +2,7 @@ import json
 import sys
 from urllib import urlopen
 
+welcome = '===== Get tracks of your 163 playlist ====='
 enterId = 'Enter the playlist id (Enter ? to get help):'
 help = 'To get the id of the playlist, go to the page\
 of it and look at the address bar.\
@@ -10,11 +11,13 @@ of it and look at the address bar.\
 errRetrive = 'No data retrived. \nPlease check the playlist id again.'
 
 while 1:
+	print(welcome)
 	playlistId = raw_input(enterId)
 
 	#change the playlistId variable 
 	if playlistId == '?':
-		print
+		print(help)
+		continue
 	urladd = "http://music.163.com/api/playlist/detail?id="\
 		+ str(playlistId) + "&updateTime=-1"
 	# Your code where you can use urlopen
@@ -38,4 +41,5 @@ while 1:
 	with open(playlistName + '.txt', 'w') as file:
 		file.write(output.encode('utf8'))
 
-	print('Success.\nCheck the directory of this file and find the .txt file!')
+	print('===== Success =====\nCheck the directory of this file and find the .txt file!')
+	print
