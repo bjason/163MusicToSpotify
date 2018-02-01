3e5894b9d67405b51d734e3318d964146cd71a8a
bjason <leiyuchu@hotmail.com>
2018-02-01 16:20:30 +0800

    Update python3.py

--- python3.py
+++ python3.py
@@ -3,21 +3,23 @@ import json
 import sys, io
 from urllib.request import urlopen
 
+welcome = '===== Get tracks of your 163 playlist ====='
 enterId = 'Enter the playlist id (Enter ? to get help):'
-help = 'To get the id of the playlist, go to the page\
+help = 'To get the id of the playlist, go to the page \
 of it and look at the address bar.\
  \nPlaylist id is the numbers after \
  "http://music.163.com/#/playlist?id="'
 errRetrive = 'No data retrived. \nPlease check the playlist id again.'
 
 while 1:
+	print(welcome)
 	playlistId = input(enterId)
 
 	#change the playlistId variable 
 	if playlistId == '?':
-		print
+		print(help)
 	urladd = "http://music.163.com/api/playlist/detail?id="\
-		+ str(playlistId) + "&updateTime=-1"
+		+ str(playlistId)
 	# Your code where you can use urlopen
 	with urlopen(urladd) as url:
 		response = url.read().decode('utf-8')
@@ -37,8 +39,8 @@ while 1:
 		output += trackName + ' - ' + artist + '\n'
 	playlistName = data["result"]["name"]
 
-
 	with open(playlistName+'.txt', 'w',encoding='utf-8') as file:
 		file.write(output)
 
-	print('Success.\nCheck the directory of this file and find the .kgl file!')
+	print('=== Success ===\nCheck the directory of this file and find the .kgl file!')
+	print
