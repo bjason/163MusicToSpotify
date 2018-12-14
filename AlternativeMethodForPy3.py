b3a4c7ee4e4c58fe6416197007e9812a96789ead
bjason <leiyuchu@hotmail.com>
2018-12-14 01:25:27 -0800

    Update AlternativeMethodForPy3.py

--- AlternativeMethodForPy3.py
+++ AlternativeMethodForPy3.py
@@ -1,52 +1,59 @@
-# -*- coding: utf-8 -*-
 import json
-import sys, io
-from urllib import request
+import sys
+import urllib.request
 
-welcome = '===== Get tracks of your 163 playlist ====='
-enterId = 'Enter the playlist id (Enter ? to get help):'
-help = 'To get the id of the playlist, go to the page \
+# using API provided by dongyonghui(https://github.com/mrdong916)
+# instruction page: www.dongyonghui.com/default/20180128-网易云、酷狗、QQ音乐歌单接口API.html
+
+welcome = "===== Get tracks of your 163 playlist ====="
+enterId = "Enter the playlist id (Enter ? to get help):"
+help = "To get the id of the playlist, go to the page\
 of it and look at the address bar.\
  \nPlaylist id is the numbers after \
- "http://music.163.com/#/playlist?id="'
-errRetrive = 'No data retrived. \nPlease check the playlist id again.'
+ 'http://music.163.com/#/playlist?id='"
+errRetrive = "No data retrived. \nPlease check the playlist id again."
 
 while 1:
 	print(welcome)
 	playlistId = input(enterId)
 
 	#change the playlistId variable 
-	if playlistId == '?':
+	if playlistId == "?":
 		print(help)
 		continue
-	urladd = "http://music.163.com/api/playlist/detail?id="\
-		+ str(playlistId)
-
-	head = {}
-	head['User-Agent'] =\
-     'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19'
-	req = request.Request(urladd, headers=head)
-    
-	with request.urlopen(urladd) as url:
-		response = url.read().decode('utf-8')
+		
+	# transCode 020111 assigns to retrieve playlist from 163 music
+	postContent = {"TransCode":"020111","OpenId":"123456789","Body":{"SongListId":playlistId}}
+	encodedContent = json.dumps(postContent).encode('utf-8')
+	
+	reqUrl = "https://api.hibai.cn/api/index/index"
+	
+	req = urllib.request.Request(reqUrl)
+	req.add_header('Content-Type', 'application/json; charset=utf-8')
+	req.add_header('Accept', 'application/json')
+	
+	response = urllib.request.urlopen(req, encodedContent).read().decode('utf-8')
 	data = json.loads(response)
-
+	
 	output = ""
 
-	if "result" not in data:
+	if data["ErrCode"] != "OK":
+		print(data["ErrCode"])
 		print(errRetrive)
 		print(help)
 		continue
 		
-	tracks = data["result"]["tracks"]
+	body = data["Body"]
+	playlistName = body["name"]
+	tracks = body["songs"]
+	
 	for track in tracks:
-		trackName = track["name"]
-		artist = track["artists"][0]["name"]
-		output += trackName + ' - ' + artist + '\n'
-	playlistName = data["result"]["name"]
-
-	with open(playlistName + '.txt', 'w', encoding='utf-8') as file:
+		trackName = track["title"]
+		artist = track["author"]
+		output += trackName + " - " + artist + "\n" 
+	
+	with open(playlistName + ".txt", "w", encoding="utf-8") as file:
 		file.write(output)
 
-	print('=== Success ===\nCheck the directory of this file and find the .txt file!')
+	print("===== Success =====\nCheck the directory of this file and find the .txt file!")
 	print
