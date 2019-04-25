d70101050350074048c1bcb33f805109f2d3bd4b
bjason <leiyuchu@hotmail.com>
2019-04-25 00:48:58 -0700

    update page

--- js/main.js
+++ js/main.js
@@ -69,7 +69,6 @@ jQuery(document).ready(function ($) {
 	});
 });
 
-var reqUrl = "https://api.hibai.cn/api/index/index";
 
 function searchPlaylist() {
 	var url = $('#playlistUrl').val();
@@ -82,53 +81,104 @@ function searchPlaylist() {
 		let index = url.indexOf("=");
 		var playlistId = url.substring(index + 1);
 
-		var postContent = {
-			"TransCode": "020111", // change to support more
-			"OpenId": "123456789",
-			"Body": {
-				"SongListId": playlistId.toString()
-			}
-		};
+		getPlaylistInfo(playlistId, "020111");
+	} else if (url.includes("y.qq.com/n/yqq/playlist/")) {
+		let start = url.indexOf('playlist/');
+		let end = url.indexOf('.html');
+		var playlistId = url.substring(start + 9, end);
 
-		$.ajax(reqUrl, {
-			data: JSON.stringify(postContent),
-			contentType: 'application/json',
-			type: 'POST',
-			accept: 'application/json',
-			beforeSend: function () {},
-			complete: function () {
-				resetButtonText(searchButton);
-			},
-			success: function (data) {
-				console.log(data);
-
-				if (data.ErrCode != "OK") {
-
-				} else {
-					let body = data.Body;
-					var playlist_name = body.name;
-					var tracks = body.songs;
-
-					let res = [];
-
-					tracks.forEach(track => {
-						var title = track.title;
-						var artist = track.author;
-
-						res.push(title + '-' + artist);
-					});
-					res = res.join('\n');
-					$('#playlistName').text(playlist_name)
-					$('#playlistContent').val(res)
-				}
-			}
-		})
+		getPlaylistInfo(playlistId, "020331");
 	} else {
 		// input error
 		$('#playlistUrl').val("");
 		$('#playlistUrl').css("background", "pink");
 		shakeElement($('#urlContainer'));
 	}
+}
+
+function getPlaylistInfo(playlistId, TransCode) {
+	var reqUrl = "https://api.hibai.cn/api/index/index";
+	var postContent = {
+		"TransCode": TransCode,
+		//"020111 for 163
+		// 020221 for kugou
+		// 020331 for qq music
+		"OpenId": "123456789",
+		"Body": {
+			"SongListId": playlistId
+		}
+	};
+
+	$.ajax(reqUrl, {
+		data: JSON.stringify(postContent),
+		contentType: 'application/json',
+		type: 'POST',
+		accept: 'application/json',
+		complete: function () {
+			var searchButton = $('#searchBtn');
+			resetButtonText(searchButton);
+		},
+		success: function (data) {
+			if (data.ErrCode != "OK") {
+				let err_info = $("#errInfo");
+				switch (data.ErrCode) {
+					case "1001":
+						err_info.html("网易云歌单ID不存在");
+						break;
+					case "1003":
+						err_info.html("网易云音乐歌单不存在");
+						break;
+					case "3001":
+						err_info.html("QQ音乐歌单ID不存在");
+						break;
+					case "3003":
+						err_info.html("QQ音乐歌单不存在");
+						break;
+					default:
+						err_info.html("出错了！")
+						break;
+				}
+
+				fadeoutError();
+			} else {
+				let body = data.Body;
+
+				if (TransCode == "020331") { // QQMUSIC
+					$("#plname").html("歌单ID");
+					var playlist_name = playlistId;
+					var tracks = body;
+				} else {
+					$("#plname").html("歌单名");
+					var playlist_name = body.name;
+					var tracks = body.songs;
+				}
+
+				let res = [];
+
+				tracks.forEach(track => {
+					var title = track.title;
+					var artist = track.author;
+
+					res.push(title + '-' + artist);
+				});
+				res = res.join('\n');
+				$('#playlistName').text(playlist_name)
+				$('#playlistContent').val(res)
+			}
+		},
+		error: function (request) {
+			let err_info = $("#errInfo");
+			err_info.html("连接错误，请稍后重试");
+
+			fadeoutError();
+		}
+	})
+}
+
+function fadeoutError() {
+	setTimeout(() => {
+		$("#errInfo").fadeOut();
+	}, 3000);
 }
 
 function shakeElement(element) {
